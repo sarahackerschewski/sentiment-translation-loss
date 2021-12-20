e0a1ba4f4c78969a349208d7e5d4817247ce9b5e
sarahackerschewski <sarah.ackerschewski@student.uni-tuebingen.de>
2021-12-20 21:54:10 +0100

    adding some preprocessing

--- src/sentiment-analysis.py
+++ src/sentiment-analysis.py
@@ -3,10 +3,14 @@
 """
 import random
 import csv
+import numpy as np
 from os import listdir
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+import re
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.lancaster import LancasterStemmer
+from sklearn.feature_selection import chi2
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectFpr
 from sklearn.preprocessing import LabelEncoder
 from sklearn import svm
 from sklearn.metrics import precision_recall_fscore_support
@@ -44,24 +48,33 @@ def read_train_data(filename_s):
     return (sequences, labels)
 
 
-def read_test_data_ger(filename):
+def preprocess(sequences):
     """
 
-    :return: the sentence tokenized file content (list)
+    :return:
     """
-    text = []
-    with open(filename, "r") as file:
-        reader = csv.reader(file, delimiter="\t")
-        for row in reader:
-            text.append(row[2])
-    tokenized = sent_tokenize(text)
-"""
-    # translator can only deal with max. 500000 characters
-    while sum(len(i) for i in tokenized) > 500000:
-        random.shuffle(tokenized)
-        tokenized = tokenized[:-1]
+    preprocessed_sequences = []
+    wn_lem = WordNetLemmatizer()
+    for sequence in sequences:
+        # remove any HTML tags
+        sequence = re.sub(r'<.*?>', '', sequence)
+        sequence = re.sub(r'\d+', '', sequence)
 
-    return tokenized"""
+        new_seq = ""
+        for i, word in enumerate(sequence.split()):
+            # stemmer or lemmatizer ?? => so far better results with lemmatizer
+            if i == 0:
+                new_seq += wn_lem.lemmatize(word)
+            else:
+                new_seq += " " + wn_lem.lemmatize(word)
+
+        preprocessed_sequences.append(new_seq)
+
+    return preprocessed_sequences
+
+def _replace_abbrevations(word):
+    common_abbr = {}
+    #if word in common_abbr.keys():
 
 
 def translate(sentences):
@@ -70,34 +83,37 @@ def translate(sentences):
     :param filename:
     :return:
     """
+    # translator can only deal with max. 500000 characters
+    while sum(len(i) for i in sentences) > 500000:
+        random.shuffle(sentences)
+        sentences = sentences[:-1]
 
+    return sentences
 
 def sentiment_classifier(x_train, y_train, x_test):
     """
 
     :return:
     """
-    """cv = CountVectorizer(ngram_range=(1, 3))
-    cv.fit(x_train)
 
-    enc_x_train = cv.transform(x_train)
-    enc_x_test = cv.transform(x_test)
-
-    tfidf = TfidfTransformer()
-    tfidf.fit(enc_x_train)
-    enc_x_train = tfidf.transform(enc_x_train).toarray()
-    enc_x_test = tfidf.transform(enc_x_test).toarray()"""
     # no stop words removing as they contain important words for sentiment like negation words
     # and thus especially important for a lexical task such as sentiment analysis
+
     tfidf = TfidfVectorizer(ngram_range=(1, 3))
     tfidf.fit(x_train)
-    enc_x_train = tfidf.transform(x_train).toarray()
-    enc_x_test = tfidf.transform(x_test).toarray()
-
+    enc_x_train = tfidf.transform(x_train)
+    enc_x_test = tfidf.transform(x_test)
+    print(enc_x_train.shape)
     le = LabelEncoder()
     enc_y_train = le.fit_transform(y_train)
 
-    classifier = svm.LinearSVC(C=0.89)
+    # feature selection
+    chi_sq = SelectFpr(chi2, alpha=0.95)
+    enc_x_train = chi_sq.fit_transform(enc_x_train, enc_y_train)
+    enc_x_test = chi_sq.transform(enc_x_test)
+    print(enc_x_train.shape)
+
+    classifier = svm.LinearSVC(C=0.425)
     classifier.fit(enc_x_train, enc_y_train)
 
     return le.inverse_transform(classifier.predict(enc_x_test))
@@ -122,7 +138,7 @@ def evaluate(gold, predicted, verbose=True):
         print("Precision (macro-average): ", precision_macro)
         print("Recall (macro-average): ", recall_macro)
         print("F1-Score (macro-average): ", f1_macro)
-        print("Accuracy (macro-average): ", acc_macro)
+        print("Accuracy: ", acc_macro)
 
         print()
 
@@ -138,15 +154,17 @@ def evaluate(gold, predicted, verbose=True):
 
     return precision_macro, recall_macro, f1_macro
 
-def create_test_data():
-    neg_files = listdir("../data/English_test_data/neg/")
+
+def create_test():
+    # read negative reviews
+    neg_files = listdir("../data/English_test/neg/")
     random.shuffle(neg_files)
     neg_files = neg_files
     neg, text = [], []
 
     for file in neg_files:
         if len(neg) <= 5000:
-            file = "../data/English_test_data/neg/" + file
+            file = "../data/English_test/neg/" + file
             print(file)
             content = open(file, "r", encoding="UTF-8").read()
             text.append(content)
@@ -159,14 +177,15 @@ def create_test_data():
 
     label_neg = ["negative" for i in range(len(neg))]
 
-    pos_files = listdir("../data/English_test_data/pos/")
+    # read positive reviews
+    pos_files = listdir("../data/English_test/pos/")
     random.shuffle(pos_files)
     pos_files = pos_files
     pos, text = [], []
 
     for file in pos_files:
         if len(pos) <= 5000:
-            file = "../data/English_test_data/pos/" + file
+            file = "../data/English_test/pos/" + file
             print(file)
             content = open(file, "r", encoding="UTF-8").read()
             text.append(content)
@@ -179,19 +198,20 @@ def create_test_data():
 
     label_pos = ["positive" for i in range(len(pos))]
 
-    test_data = neg + pos
+    test = neg + pos
     gold = label_neg + label_pos
-
-    randomized = list(zip(test_data, gold))
+    randomized = list(zip(test, gold))
     random.shuffle(randomized)
+    test, gold = zip(*randomized)
 
-    test_data, gold = zip(*randomized)
+    # write file with tokenized English test data
     with open('../data/test_movie_reviews_english_tokenized.csv', 'wt', encoding="UTF-8") as out_file:
         csv_writer = csv.writer(out_file, delimiter=";")
-        for i, sent in enumerate(test_data):
+        for i, sent in enumerate(test):
             csv_writer.writerow([gold[i], sent])
 
-    test_data = []
+    # read German reviews
+    test = []
     with open("../data/test_movie_reviews_german.tsv", "r", encoding="UTF-8") as file:
         reader = csv.reader(file, delimiter="\t")
         for row in reader:
@@ -199,22 +219,48 @@ def create_test_data():
                 tokenized = sent_tokenize(row[2], language="german")
 
                 for sent in tokenized:
-                    test_data.append(sent)
-    random.shuffle(test_data)
+                    test.append(sent)
+    random.shuffle(test)
 
+    # write file with tokenized German test data
     with open('../data/test_movie_reviews_german_tokenized.csv', 'w', encoding="UTF-8") as out_file:
         csv_writer = csv.writer(out_file, delimiter=';')
-        for sent in test_data:
+        for sent in test:
             print(sent)
             csv_writer.writerow([sent])
 
+
 if __name__ == '__main__':
 
-    seq, labels = read_train_data(["../data/training_movie_reviews.txt"])
+    # read training data
+    train, labels = read_train_data(["../data/training_movie_reviews.txt"])
 
-    # test on English movie reviews
+    # read test data from English movie reviews
+    test, gold = [], []
+    neg, pos = 0, 0
+    with open("../data/test_movie_reviews_english_tokenized.csv") as f:
+        csv_reader = csv.reader(f, delimiter=";")
 
-    #pred = sentiment_classifier(seq, labels, test_data)
-    #evaluate(gold, pred)
+        for row in csv_reader:
+            if len(row) == 2:
+                if row[0] == "negative" and neg < 1000:
+                    neg += 1
+                    gold.append(row[0])
+                    test.append(row[1])
+
+                elif row[0] == "positive" and pos < 1000:
+                    pos += 1
+                    gold.append(row[0])
+                    test.append(row[1])
+
+    # pre-processing
+    train = preprocess(train)
+    test = preprocess(test)
+    
+    # classification
+    pred = sentiment_classifier(train, labels, test)
+
+    # evaluation
+    evaluate(gold, pred)
 
 
